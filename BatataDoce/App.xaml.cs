be198f0dbed45be4fa05157284c085efcbae2713
ArthurCostaVieira <opsarthur@gmail.com>
2024-03-04 08:14:44 -0300

    p/ Ola, esse negocio vale nota, e é 1º comit :>

---
+++ BatataDoce/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace BatataDoce;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ BatataDoce/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace BatataDoce;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
