f8382e0f27495f690ae66ade9b08d0c25b155fcc
ArthurCostaVieira <opsarthur@gmail.com>
2024-03-06 09:48:59 -0300

    Um monte de coisas, incluindo a tela inicial

--- BatataDoce/MainPage.xaml.cs
+++ BatataDoce/MainPage.xaml.cs
@@ -8,5 +8,21 @@ public partial class MainPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+	void QuandoClicarNoBotao(object sender, EventArgs args)
+	{
+		Application.Current.MainPage = new GamePage();
+	}
+
+		void QuandoClicarNoBotaoSobre(object sender, EventArgs args)
+	{
+		Sobre.IsVisible=true;
+	}
+
+		void QuandoClicarNoBotaoVoltar(object sender, EventArgs args)
+	{
+		Sobre.IsVisible=false;
+	}
+
 }
 
